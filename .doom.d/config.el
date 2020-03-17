c68aea1a34c7997ddc43cc68293d1dfd3d2eaa20
Roger Gonzalez <rogergonzalez21@gmail.com>
2020-03-17 18:49:29 -0300

    Cleaned up code a bit

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -15,51 +15,51 @@
 (setq user-full-name "Roger Gonzalez"
       user-mail-address "roger@rogs.me")
 
-                  ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-                  ;; are the three important ones:
-                  ;;
-                  ;; + `doom-font'
-                  ;; + `doom-variable-pitch-font'
-                  ;; + `doom-big-font' -- used for `doom-big-font-mode'
-                  ;;
-                  ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-                  ;; font string. You generally only need these two:
-                  ;; test
-                  (setq doom-font (font-spec :family "monospace" :size 28)
-                        doom-variable-pitch-font (font-spec :family "sans"))
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; test
+(setq doom-font (font-spec :family "monospace" :size 28)
+      doom-variable-pitch-font (font-spec :family "sans"))
 
-                  ;; There are two ways to load a theme. Both assume the theme is installed and
-                  ;; available. You can either set `doom-theme' or manually load a theme with the
-                  ;; `load-theme' function. These are the defaults.
-                  (setq doom-theme 'doom-one)
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-oceanic-next)
 
-                  ;; If you intend to use org, it is recommended you change this!
-                  (setq org-directory "~/org")
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org")
 
-                  ;; If you want to change the style of line numbers, change this to `relative' or
-                  ;; `nil' to disable it:
-                  (setq display-line-numbers-type t)
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
 
 
-                  ;; Here are some additional functions/macros that could help you configure Doom:
-                  ;;
-                  ;; - `load!' for loading external *.el files relative to this one
-                  ;; - `use-package' for configuring packages
-                  ;; - `after!' for running code after a package has loaded
-                  ;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-                  ;;   looks when you load packages with `require' or `use-package'.
-                  ;; - `map!' for binding new keys
-                  ;;
-                  ;; To get information about any of these functions/macros, move the cursor over
-                  ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-                  ;; This will open documentation for it, including demos of how they are used.
-                  ;;
-                  ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-                  ;; they are implemented.
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
 
-                  ;; Set emacs logo as the default image
-                  (setq
-                   +doom-dashboard-banner-file (expand-file-name "logo.png" doom-private-dir))
+;; Set emacs logo as the default image
+(setq
+ +doom-dashboard-banner-file (expand-file-name "logo.png" doom-private-dir))
 
 ;; Saves emacs backups in other folder
 (setq
