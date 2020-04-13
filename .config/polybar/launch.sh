e1ccf7bc5aa5df8fb7014d7673225a8167d716df
Roger Gonzalez <roger@rogs.me>
2020-04-13 17:31:21 -0300

    Changed polybar configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,6 +13,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar --config=$HOME/.config/polybar/config top &
-polybar --config=$HOME/.config/polybar/config bottom &
 
 echo "Polybar launched..."
