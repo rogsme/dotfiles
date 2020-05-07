733066aeb560f8553731596cbf7a26e1ae8ce5c3
Roger Gonzalez <roger@rogs.me>
2020-05-07 11:47:49 -0300

    Working on the rogs theme for qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1373,7 +1373,7 @@ c.qt.highdpi = True
 ##   - tr-TR: Turkish (Turkey)
 ##   - uk-UA: Ukrainian (Ukraine)
 ##   - vi-VN: Vietnamese (Viet Nam)
-# c.spellcheck.languages = []
+c.spellcheck.languages = ['en-US', 'es-ES']
 
 ## Hide the statusbar unless a message is shown.
 ## Type: Bool
