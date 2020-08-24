d8ddccd8859ec8ae40173bba6d1da44119960ced
Roger Gonzalez <roger@rogs.me>
2020-08-24 11:25:03 -0300

    Changed resolution to 1920x1080, now supporting multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,6 +12,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --config=$HOME/.config/polybar/config top &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --config=$HOME/.config/polybar/config top &
+done
 
 echo "Polybar launched..."
