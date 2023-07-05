3a6ba949c317c9dc2e0f1f30a71902a50a10ca68
Roger Gonzalez <roger@rogs.me>
2023-07-05 17:06:13 -0300

    Upgraded config for new Dell laptop

--- .config/fish/functions/ua-vpn.fish
+++ .config/fish/functions/ua-vpn.fish
@@ -1,6 +1,6 @@
 function ua-vpn
-         pritunl-client stop 11afcmf68bjxhyvl
+         pritunl-client stop uzlo5uosuo0h5uck
          mullvad disconnect
-         pritunl-client start 11afcmf68bjxhyvl
+         pritunl-client start uzlo5uosuo0h5uck
          pritunl-client list
 end
