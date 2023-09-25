8c10e7e3c191dcf5664f4579d120bd6e37707533
Roger Gonzalez <roger@rogs.me>
2023-09-25 17:08:25 -0300

    Updated screenlock function

--- .config/i3/i3lock_script.sh
+++ .config/i3/i3lock_script.sh
@@ -5,13 +5,7 @@
 # |_|  \___/ \__, |___/    https://git.rogs.me
 #            |___/
 #
-keepassxc --lock
-#if pidof openvpn; then
-    #notify-send "Shutting down VPN"
-    #pritunl-client stop r1e5vfvldadchscs
-#fi
 scrot /tmp/screen.png
 xwobf -s 11 /tmp/screen.png
 i3lock -i /tmp/screen.png
-#i3lock -c 000000
 rm /tmp/screen.png
