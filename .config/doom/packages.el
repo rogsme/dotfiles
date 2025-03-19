28b3b44ca760a8460347d37319f5771646a961b5
Roger Gonzalez <roger@rogs.me>
2025-03-19 14:54:00 -0300

    Added my fork of magit-gptcommit

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -72,7 +72,11 @@
 (package! ox-slack)
 (package! org-auto-tangle)
 (package! auto-virtualenv)
-(package! magit-gptcommit)
+(package! magit-gptcommit
+  :recipe (:host github
+           :repo "rogsme/magit-gptcommit"
+           :branch "abort-gptcommit-if-buffer-gets-killed"))
 (package! forge-llm
   :recipe (:host gitlab :repo "rogs/forge-llm"))
 (package! llm)
+(package! cov)
