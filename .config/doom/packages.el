802da7afa10da2683dd651a25924658060e9e72f
Roger Gonzalez <roger@rogs.me>
2025-03-31 09:07:29 -0300

    Changed forge-llm to melpa and fixed bug with ws-butler

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -73,8 +73,13 @@
 (package! org-auto-tangle)
 (package! auto-virtualenv)
 (package! magit-gptcommit)
-(package! forge-llm
-  :recipe (:host gitlab :repo "rogs/forge-llm"))
+(package! forge-llm)
 (package! llm)
 (package! cov)
 (package! aidermacs)
+(package! ws-butler
+  :recipe (:host github
+           :repo "emacsmirror/nongnu_elpa"
+           :branch "elpa/ws-butler"
+           :local-repo "ws-butler")
+  :pin "9ee5a7657a22e836618813c2e2b64a548d27d2ff")
