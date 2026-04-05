4720fb445931a704c61cb2f895f51e6150986065
Roger Gonzalez <roger@rogs.me>
2026-04-05 20:49:02 -0300

    Improved the handy toggle script

--- .config/i3/scripts/handy-toggle.sh
+++ .config/i3/scripts/handy-toggle.sh
@@ -2,13 +2,29 @@
 # ~/.local/bin/handy-toggle.sh
 
 STATE_FILE="/tmp/handy-recording"
+APP_NAME="Handy"
+STACK_TAG="handy-toggle"
+
+notify_state() {
+    local icon="$1"
+    local body="$2"
+
+    notify-send \
+        -a "$APP_NAME" \
+        -i "$icon" \
+        -u low \
+        -t 3500 \
+        -h "string:x-dunst-stack-tag:$STACK_TAG" \
+        "$APP_NAME" \
+        "$body"
+}
 
 if [[ -f "$STATE_FILE" ]]; then
     handy --toggle-post-process
     rm "$STATE_FILE"
-    notify-send -u low -t 2000 "🎙️ Handy" "Transcription stopped"
+    notify_state "media-record" "Transcription stopped"
 else
     handy --toggle-post-process
     touch "$STATE_FILE"
-    notify-send -u low -t 2000 "🎙️ Handy" "Transcription started..."
+    notify_state "audio-input-microphone" "Transcription started..."
 fi
